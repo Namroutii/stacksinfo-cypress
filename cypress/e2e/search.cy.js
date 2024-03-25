2c69aaa5e8cdfdcb4f2cfd6710da80f8036ce7b9
Namrouti <s12116442@stu.najah.edu>
2024-03-25 22:40:09 +0200

    add 8 scripts , 1 fail and 7 pass

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -54,7 +54,7 @@ describe("Search Bar Component", () => {
     cy.get(".company-slogan").should("contain", "Ramallah");
   });
 
-  it('should display an error message for URL input', () => {                                                              
+  it('should display an error message for URL input', () => {
     cy.get('input[class="search-bar"]').should("be.visible").type('https://stacksinfo.web.app/').should("have.value", "https://stacksinfo.web.app/")
     cy.get('.Not-found-container').should('exist')
   })
@@ -75,30 +75,99 @@ describe("Search Bar Component", () => {
     cy.get('input[class="search-bar"]').click();
     cy.get('.last-search-and-results .header-last .remove-button').should('exist').click();
     cy.get('.last-search-and-results').should('not.exist');
-});
+  });
 
 
-it('should display an error message for number input', () => {
-  cy.get('input[class="search-bar"]').should("be.visible").type('100{enter}');
-  cy.get('.Not-found-container').should('be.visible');
-});
+  it('should display an error message for number input', () => {
+    cy.get('input[class="search-bar"]').should("be.visible").type('100{enter}');
+    cy.get('.Not-found-container').should('be.visible');
+  });
 
 
-it('should persist search results after refreshing the page', () => {
-  cy.get('input[class="search-bar"]').should("be.visible").type('React Native{enter}');
-  cy.get('.card-result').should('be.visible');
-  cy.get(".card-result .result-item").should(
-    "have.attr",
-    "title",
-    "React Native"
-  );
-  cy.reload();
-  cy.get('.result-container').should('be.visible');
-  cy.get(".card-result .result-item").should(
-    "have.attr",
-    "title",
-    "React Native"
-  );
-});
+  it('should persist search results after refreshing the page', () => {
+    cy.get('input[class="search-bar"]').should("be.visible").type('React Native{enter}');
+    cy.get('.card-result').should('be.visible');
+    cy.get(".card-result .result-item").should(
+      "have.attr",
+      "title",
+      "React Native"
+    );
+    cy.reload();
+    cy.get('.result-container').should('be.visible');
+    cy.get(".card-result .result-item").should(
+      "have.attr",
+      "title",
+      "React Native"
+    );
+  });
+
+  it('should allow typing a search query in lower case only', () => {
+    cy.visit('https://stacksinfo.web.app/')
+    cy.get('input[class="search-bar"]').should("be.visible").type("react{enter}")
+    cy.get(".card-result .result-item").should(
+      "have.attr",
+      "title",
+      "React Native"
+    );
+  })
+
+  it('should allow typing a search query in upper case only', () => {
+    cy.visit('https://stacksinfo.web.app/')
+    cy.get('input[class="search-bar"]').should("be.visible").type("REACT{enter}")
+    cy.get(".card-result .result-item").should(
+      "have.attr",
+      "title",
+      "React Native"
+    );
+  })
+
+  it('should allow typing a search query in both lower and upper cases', () => {
+    cy.visit('https://stacksinfo.web.app/')
+    cy.get('input[class="search-bar"]').should("be.visible").type("reACt{enter}")
+    cy.get(".card-result .result-item").should(
+      "have.attr",
+      "title",
+      "React Native"
+    );
+  })
+
+  it('should allow typing a partial search query', () => {
+    cy.visit('https://stacksinfo.web.app/')
+    cy.get('input[class="search-bar"]').should("be.visible").type("rea{enter}")
+    cy.get(".card-result .result-item").should(
+      "have.attr",
+      "title",
+      "React Native"
+    );
+  })
+
+  it('should have a Placeholder Text in Search Input Field', () => {
+    cy.get('input[class="search-bar"]').should("be.visible")
+    cy.get('.search-bar').should('have.attr', 'placeholder', 'Company, city, technology, language...');
+  });
+
+
+  it('should display valid output after clicking on search button', () => {
+    cy.get('input[class="search-bar"]').should("be.visible").type('Rect Native');
+    cy.get('.MuiSvgIcon-root MuiSvgIcon-fontSizeMedium search-icon css-vubbuv').click()
+    cy.get(".card-result .result-item").should(
+      "have.attr",
+      "title",
+      "React Native"
+    );
+  });
+
+  it("should display relevant cards when searching for an partial existing company", () => {
+    cy.get(".search-bar").type("AD{enter}");
+    cy.get(".card-container").should("be.visible");
+    cy.get(".company-name").should("contain", "ADHAM");
+    cy.get(".company-slogan").should("contain", "Nablus");
+  });
+
+  it("should display relevant cards when searching for an partial existing city", () => {
+    cy.get(".search-bar").type("Nabu{enter}");
+    cy.get(".card-container").should("be.visible");
+    cy.get(".company-slogan").should("contain", "Nablus");
+  });
 
 });
